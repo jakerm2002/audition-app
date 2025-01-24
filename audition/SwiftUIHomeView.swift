66424a1e15faf3c15e72ce0f92c04f50e83777b8
Jake Medina <jake.medina02@gmail.com>
2025-01-23 21:29:55 -0600

    create cards on SwiftUIHomeView for each AuditionDataModel and show a thumbnail of the drawing

--- audition/SwiftUIHomeView.swift
+++ audition/SwiftUIHomeView.swift
@@ -7,11 +7,49 @@
 
 import SwiftUI
 
+struct CardView: View {
+    
+    var image: UIImage?
+    
+    init(image: UIImage? = nil) {
+        self.image = image
+    }
+    
+    var body: some View {
+        VStack {
+            if let image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else {
+                Image(systemName: "exclamationmark.triangle.fill").imageScale(.large)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        .clipShape(.rect(cornerRadius: 12))
+        .clipped()
+        .shadow(radius: 5)
+    }
+}
+
 struct SwiftUIHomeView: View {
+    
+    let columns = [GridItem](repeating: GridItem(.flexible(), spacing: 16), count: 5)
+    
+    let models: [AuditionDataModel] = [AuditionDataModel(), AuditionDataModel(), AuditionDataModel(), AuditionDataModel(), AuditionDataModel()]
+    
     var body: some View {
         NavigationStack {
-            NavigationLink("Start") {
-                SwiftUIDrawingView().environmentObject(AuditionDataModel())
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(models) { model in
+                        if let thumbnail = model.thumbnail {
+                            CardView(image: thumbnail)
+                                .frame(height: 100)
+                        }
+                    }
+                }.padding()
             }.navigationTitle("Drawings")
         }
     }
