dfb165df6dce7592edf57af4cccf9d84ac0c62ee
Jake Medina <jake.medina02@gmail.com>
2025-01-25 17:54:32 -0600

    Make SwiftUI the new entry point of the app

---
+++ audition/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  audition
+//
+//  Created by Jake Medina on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SwiftUIHomeView()
+    }
+}
---
+++ audition/auditionApp.swift
@@ -0,0 +1,19 @@
+//
+//  auditionApp.swift
+//  audition
+//
+//  Created by Jake Medina on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct auditionApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
