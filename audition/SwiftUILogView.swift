6b1a9ee7ccf54fbbc4b8ccd31042e4ae7f4e34b9
Jake <jake.medina02@gmail.com>
2025-02-11 23:53:10 -0600

    Feature: Switch branches using the log view (#5)
    * getting a sidebar with branches appearing
    
    * figuring out how to display a selection in the detail part of a NavigationSplitView
    
    * move logic for commit log into separate view
    
    * if HEAD points to a branch, automatically select that branch in the sidebar. if in 'detached HEAD' state, show the log from the current HEAD
    
    * create button to checkout a branch
    
    * add back the custom back button that forces the PKToolPicker to appear
    
    * SwiftUILogView more verbose error handling
    
    * SwiftUILogView functions for repetitive code
    
    * don't try to truncate the navigation title if it is the name of the default branch (in the case that the default branch name changes to more than 6 characters)
    
    * rename singleSelection to commitSelection
    
    * fixed issue where clicking on a branch checkout button wouldn't update the drawing
    
    * code cleanup

--- audition/SwiftUILogView.swift
+++ audition/SwiftUILogView.swift
@@ -8,67 +8,17 @@
 import SwiftUI
 import PencilKit
 
-struct SwiftUILogView: View {
+
+struct SwiftUILogDetailView: View {
     
     @EnvironmentObject var dataModel: AuditionDataModel
+    @State private var commitSelection: String? = nil
+    
+    @Binding var commits: [Commit]
     @Binding var rendition: PKDrawing
     @Binding var updatesCounter: Int
     
-    @State var commits: [Commit] = []
-    @State private var singleSelection: String? = nil
-    
     @Environment(\.dismiss) var dismiss
-    
-    var body: some View {
-        List(commits, id: \.sha256DigestValue!, selection: $singleSelection) { commit in
-            LazyVStack(alignment: .leading) {
-                Button(action: {
-                            do {
-                                try dataModel.checkout(commit: commit.sha256DigestValue!)
-                            } catch {
-                                print("ERROR: Checking out ref failed")
-                            }
-                            setDrawingData(commit: commit)
-                            dismiss()
-                        },
-                       label: {
-                            VStack(alignment: .leading) {
-                                Text(commit.message).tint(.primary)
-                                HStack {
-                                    Text(commit.sha256DigestValue!.prefix(7)).tint(.primary)
-                                    Text(DateFormatter.localizedString(from: commit.timestamp, dateStyle: .medium, timeStyle: .medium)).tint(.primary)
-                                }
-                            }
-                })
-                .contentShape(Rectangle())
-            }
-        }
-        .navigationTitle(commits.isEmpty ? "Log" : "Commits from \(commits.first!.sha256DigestValue!.prefix(7))")
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Button {
-                    updatesCounter += 1
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.backward")
-                }
-            }
-        }
-        .overlay {
-            if commits.isEmpty {
-                ContentUnavailableView("No Commits", image: "")
-            }
-        }
-        .onAppear {
-            do {
-                commits = try dataModel.log()
-            } catch let error{
-                print("ERROR: Error showing log: \(error)")
-                commits = []
-            }
-        }
-    }
     
     func setDrawingData(commit: Commit) {
         // grab the blob that was included in the commit
@@ -84,8 +34,152 @@ struct SwiftUILogView: View {
             print("setDrawingData FAILED to get blobs: \(error)")
         }
     }
+    
+    var body: some View {
+        List(commits, id: \.sha256DigestValue!, selection: $commitSelection) { commit in
+            LazyVStack(alignment: .leading) {
+                Button(action: {
+                            do {
+                                try dataModel.checkout(commit: commit.sha256DigestValue!)
+                                setDrawingData(commit: commit)
+                                dismiss()
+                            } catch let error {
+                                print("ERROR in SwiftUILogView: Checking out ref failed: \(error)")
+                            }
+                        },
+                       label: {
+                            VStack(alignment: .leading) {
+                                Text(commit.message).tint(.primary)
+                                HStack {
+                                    Text(commit.sha256DigestValue!.prefix(7)).tint(.primary)
+                                    Text(DateFormatter.localizedString(from: commit.timestamp, dateStyle: .medium, timeStyle: .medium)).tint(.primary)
+                                }
+                            }
+                })
+                .contentShape(Rectangle())
+            }
+        }
+        .overlay {
+            if commits.isEmpty {
+                ContentUnavailableView("No Commits", image: "")
+            }
+        }
+    }
+}
+
+
+struct SwiftUILogView: View {
+    
+    @EnvironmentObject var dataModel: AuditionDataModel
+    @State var branches: [String : String] = [:]
+    @State var commits: [Commit] = []
+    @State private var sidebarSelection: String? = nil
+    
+    @Binding var rendition: PKDrawing
+    @Binding var updatesCounter: Int
+    
+    @Environment(\.dismiss) var dismiss
+    
+    func setDrawingData(commit: Commit) {
+        // grab the blob that was included in the commit
+        // we're assuming there will only be one, this will NOT BE TRUE in the future
+        // once we are committing individual strokes instead of the entire drawing
+        do {
+            let aBlob = try dataModel.showBlobs(commit: commit.sha256DigestValue!)[0]
+            let newDrawing = try PKDrawing(data: aBlob.contents)
+            rendition = newDrawing
+            updatesCounter += 1
+            print("setDrawingData succeeded")
+        } catch let error {
+            print("setDrawingData FAILED to get blobs: \(error)")
+        }
+    }
+    
+    func setCommitsFromBranch(branch: String) {
+        do {
+            commits = try dataModel.log(branch: branch)
+        } catch let error {
+            print("ERROR in SwiftUILogView: Couldn't log branch \(branch): \(error)")
+        }
+    }
+    
+    func setCommitsFromHEAD() {
+        do {
+            commits = try dataModel.log()
+        } catch let error {
+            print("ERROR in SwiftUILogView: Couldn't log HEAD \(dataModel.HEAD): \(error)")
+        }
+    }
+    
+    var body: some View {
+        NavigationSplitView(sidebar: {
+            List (branches.sorted(by: <), id: \.key, selection: $sidebarSelection) { key, value in
+                HStack {
+                    NavigationLink(key, value: key)
+                    Spacer()
+                    Button(action: {
+                        do {
+                            try dataModel.checkout(branch: key)
+                            guard let commit = dataModel.objects[value] as? Commit else {
+                                throw AuditionError.runtimeError("Branch ref does not point to a commit.")
+                            }
+                            setDrawingData(commit: commit)
+                            dismiss()
+                        } catch let error {
+                            print("ERROR in SwiftUILogView: Checking out branch failed: \(error)")
+                        }
+                    }, label: {
+                        Image(systemName: "arrow.left")
+                    })
+                    .buttonStyle(.bordered)
+                }
+            }
+            .navigationTitle("Branches")
+            .onAppear {
+                branches = dataModel.branches
+            }
+        }, detail: {
+            // HEAD points to a branch that has been committed to
+            if let branchName = sidebarSelection {
+                SwiftUILogDetailView(commits: $commits, rendition: $rendition, updatesCounter: $updatesCounter)
+                    .navigationTitle(branchName)
+                    .onAppear { setCommitsFromBranch(branch: branchName) }
+                    .onChange(of: branchName) { setCommitsFromBranch(branch: branchName) }
+            }
+            // HEAD points to a commit
+            else if dataModel.objects[dataModel.HEAD] is Commit {
+                SwiftUILogDetailView(commits: $commits, rendition: $rendition, updatesCounter: $updatesCounter)
+                    .navigationTitle(String(dataModel.HEAD.prefix(7)))
+                    .onAppear { setCommitsFromHEAD() }
+            }
+            // HEAD points to a branch with no commits (it's probably the default branch, 'main')
+            else {
+                SwiftUILogDetailView(commits: $commits, rendition: $rendition, updatesCounter: $updatesCounter)
+                    .navigationTitle(String(dataModel.HEAD))
+                    .onAppear { setCommitsFromHEAD() }
+            }
+        })
+        .onAppear {
+            if let currentBranch = dataModel.currentBranch {
+                sidebarSelection = currentBranch
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    updatesCounter += 1
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                }
+            }
+        }
+
+    }
 }
 
 #Preview {
-    SwiftUILogView(rendition: Binding.constant(PKDrawing()), updatesCounter: Binding.constant(0))
+    var model: AuditionDataModel = generateSampleDataThreeCommits()
+    SwiftUILogView(rendition: Binding.constant(PKDrawing()), updatesCounter: Binding.constant(0)).environmentObject(model)
 }
