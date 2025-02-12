3afe924a8ac067e56c1286b2261dbd082d5cf6cb
Jake Medina <jake.medina02@gmail.com>
2025-02-11 19:37:33 -0600

    figuring out how to display a selection in the detail part of a NavigationSplitView

--- audition/SwiftUILogView.swift
+++ audition/SwiftUILogView.swift
@@ -8,6 +8,64 @@
 import SwiftUI
 import PencilKit
 
+/*
+struct SwiftUILogDetailView: View {
+    
+    @EnvironmentObject var dataModel: AuditionDataModel
+    
+    var body: some View {
+        List(commits, id: \.sha256DigestValue!, selection: $singleSelection) { commit in
+            LazyVStack(alignment: .leading) {
+                Button(action: {
+                            do {
+                                try dataModel.checkout(commit: commit.sha256DigestValue!)
+                            } catch {
+                                print("ERROR: Checking out ref failed")
+                            }
+                            setDrawingData(commit: commit)
+                            dismiss()
+                        },
+                       label: {
+                            VStack(alignment: .leading) {
+                                Text(commit.message).tint(.primary)
+                                HStack {
+                                    Text(commit.sha256DigestValue!.prefix(7)).tint(.primary)
+                                    Text(DateFormatter.localizedString(from: commit.timestamp, dateStyle: .medium, timeStyle: .medium)).tint(.primary)
+                                }
+                            }
+                })
+                .contentShape(Rectangle())
+            }
+        }
+        .navigationTitle(commits.isEmpty ? "Log" : "Commits from \(commits.first!.sha256DigestValue!.prefix(7))")
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    updatesCounter += 1
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                }
+            }
+        }
+        .overlay {
+            if commits.isEmpty {
+                ContentUnavailableView("No Commits", image: "")
+            }
+        }
+        .onAppear {
+            do {
+                commits = try dataModel.log()
+            } catch let error{
+                print("ERROR: Error showing log: \(error)")
+                commits = []
+            }
+        }
+    }
+}
+ */
+
 struct SwiftUILogView: View {
     
     @EnvironmentObject var dataModel: AuditionDataModel
@@ -17,70 +75,27 @@ struct SwiftUILogView: View {
     @State var branches: [String : String] = [:]
     @State var commits: [Commit] = []
     @State private var singleSelection: String? = nil
+    @State private var sidebarSelection: String? = nil
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
         NavigationSplitView(sidebar: {
-            List {
-                ForEach(branches.sorted(by: >), id: \.key) { key, value in
-                    NavigationLink(key) {
-                        
-                    }
-                }
+            List (branches.sorted(by: >), id: \.key, selection: $sidebarSelection) { key, value in
+                NavigationLink(key, value: key)
             }
             .navigationTitle("Branches")
             .onAppear {
                 branches = dataModel.branches
             }
         }, detail: {
-            List(commits, id: \.sha256DigestValue!, selection: $singleSelection) { commit in
-                LazyVStack(alignment: .leading) {
-                    Button(action: {
-                                do {
-                                    try dataModel.checkout(commit: commit.sha256DigestValue!)
-                                } catch {
-                                    print("ERROR: Checking out ref failed")
-                                }
-                                setDrawingData(commit: commit)
-                                dismiss()
-                            },
-                           label: {
-                                VStack(alignment: .leading) {
-                                    Text(commit.message).tint(.primary)
-                                    HStack {
-                                        Text(commit.sha256DigestValue!.prefix(7)).tint(.primary)
-                                        Text(DateFormatter.localizedString(from: commit.timestamp, dateStyle: .medium, timeStyle: .medium)).tint(.primary)
-                                    }
-                                }
-                    })
-                    .contentShape(Rectangle())
-                }
-            }
-            .navigationTitle(commits.isEmpty ? "Log" : "Commits from \(commits.first!.sha256DigestValue!.prefix(7))")
-            .navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        updatesCounter += 1
-                        dismiss()
-                    } label: {
-                        Image(systemName: "chevron.backward")
+            if let branchName = sidebarSelection {
+                Text(branchName)
+                    .onChange(of: branchName) {
+                        print("branch changed")
                     }
-                }
-            }
-            .overlay {
-                if commits.isEmpty {
-                    ContentUnavailableView("No Commits", image: "")
-                }
-            }
-            .onAppear {
-                do {
-                    commits = try dataModel.log()
-                } catch let error{
-                    print("ERROR: Error showing log: \(error)")
-                    commits = []
-                }
+            } else {
+                
             }
         })
 
